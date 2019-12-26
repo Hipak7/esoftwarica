18adf1920a48623bb111a8fe73a81c7aca79c7b3
hipak <hpk393@gmail.com>
2019-12-26 22:39:32 +0545

    Class and Adapters for Students Added!

--- app/src/main/java/com/novc21/esoftwarica/adapter/StudentsAdapter.java
+++ app/src/main/java/com/novc21/esoftwarica/adapter/StudentsAdapter.java
@@ -39,7 +39,51 @@ public class StudentsAdapter extends RecyclerView.Adapter<StudentsAdapter.Studen
 
     @SuppressLint("SetTextI18n")
     @Override
+    public void onBindViewHolder(@NonNull StudentsViewHolder holder, final int position) {
+        final Students students = studentsList.get(position);
+        holder.tvName.setText(students.getName());
+        holder.tvAge.setText(students.getAge()+"");
+        holder.tvAddress.setText(students.getAddress());
+        holder.tvGender.setText(students.getGender());
+        holder.imgDelete.setImageDrawable(context.getResources().getDrawable(R.drawable.ic_delete_black_24dp));
+        holder.imgEdit.setImageDrawable(context.getResources().getDrawable(R.drawable.ic_edit_black_24dp));
+        String gender = students.getGender();
+        if (gender == "male") {
+            holder.imgProfile.setImageResource(R.drawable.male);
+        } else if (gender == "female") {
+            holder.imgProfile.setImageResource(R.drawable.female);
+        } else {
+            holder.imgProfile.setImageResource(R.drawable.other);
+        }
 
+        holder.imgProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "This is : "+ students.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        holder.imgDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Students delStudents = studentsList.get(position);
+                studentsList.remove(position);
+                notifyItemRemoved(position);
+                Toast.makeText(context, delStudents + "is removed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        holder.imgEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Students edtStudents = studentsList.get(position);
+                int index = studentsList.indexOf(edtStudents);
+                EditStudent.index = index;
+                Intent intent = new Intent(context, EditStudent.class);
+                context.startActivity(intent);
+            }
+        });
+    }
 
     @Override
     public int getItemCount() {
